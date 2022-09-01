0ac2907c61900a67284689cef8adaa58e384dac6
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-09-01 18:20:19 +0200

    fixed visibility issues

--- prog_vue/js/script.js
+++ prog_vue/js/script.js
@@ -3,12 +3,23 @@ Vue.config.devtools = true
 const app = new Vue({
     el: '#root',
     data: {
-        discs: []
+        discs: [],
+        genres: [],
+        filteredDiscs: []
     },
     mounted() {
-        axios.get('http://localhost/php-ajax-dischi/prog_php')
+        axios.get('../prog_php/api/discs.php')
         .then((res)=>{
             this.discs = res.data
+            this.fetchGenres()
         })
+    },
+    methods: {
+        fetchGenres() {
+            this.discs.forEach(disc => {
+                if(!this.genres.includes(disc.genre)) this.genres.push(disc.genre)
+            });
+            return this.genres
+        },
     }
 })
